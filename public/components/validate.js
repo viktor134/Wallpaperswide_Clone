d05aed75bf1bc65504d2169f68c7bfaa62619211
Victor <tricika96@gmail.com>
2021-03-21 11:05:46 -0700

    edit validate.js

--- public/components/validate.js
+++ public/components/validate.js
@@ -1,14 +1,18 @@
-document.addEventListener('DOMContentLoaded',function () {
-    const name=document.getElementById('name'),
-        email=document.getElementById('email'),
-        password=document.getElementById('password'),
-        confirm_password=document.getElementById('confirm'),
-        onsubmit=document.getElementById('submit');
+document.addEventListener('DOMContentLoaded', function () {
+    const name = document.getElementById('name'),
+        email = document.getElementById('email'),
+        password = document.getElementById('password'),
+        confirm_password = document.getElementById('confirm'),
+        onsubmit = document.getElementById('submit'),
+        form = document.getElementById('form');
 
-    console.log(name);
-    console.log(email);
-    console.log(password);
-    console.log(confirm_password);
 
-});
+
+
+
+
+
+
+
+
 
--- public/layouts/page.php
+++ public/layouts/page.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script defer src="https://use.fontawesome.com/releases/v5.0.0/js/all.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+
 
 </head>
 <body>
